a29c4a79ca664748f5fb0d73e2a49a81edcfd9b0
AndrewAvicom <40204198+AvicomAvicor@users.noreply.github.com>
2018-06-12 16:02:27 +0300

    Create myProg01.cpp

---
+++ myProg01.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+void frint(const char *str);
+
+
+int main()
+{
+	frint("String for Probe");
+	getchar();
+    return 0;
+}
+
+void frint(const char *str) {
+	while (*str)
+	{
+		if (*str == ' ') printf("%c", '-');
+		else printf("%c", *str);
+		str++;
+
+	}
+
+}
